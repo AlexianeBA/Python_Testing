83b217bbc637c66c61329862ff5b09304e4ca12c
AlexianeBA <barbealexiane05@gmail.com>
2024-02-02 16:05:30 +0100

    test login/logout

--- tests/unit_tests/test_login.py
+++
@@ -1,8 +0,0 @@
-from server import app
-
-class TestLogin:
-    client = app.test_client()
-    
-    def test_login(self):
-        result = self.client.get('/')
-        assert result.status_code == 200
