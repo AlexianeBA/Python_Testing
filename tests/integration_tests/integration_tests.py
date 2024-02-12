2aabf21df25d6af92bad9f901ceb3a299316f1d5
AlexianeBA <barbealexiane05@gmail.com>
2024-02-12 11:01:48 +0100

    fix integration test

--- tests/integration_tests/integration_tests.py
+++ tests/integration_tests/integration_tests.py
@@ -9,11 +9,10 @@ def client():
 
 
 def test_integration(client):
-    # Test de connexion avec un e-mail valide
-    login_response = client.post("/showSummary", data={"email": "john@simplylift.co"})
-    assert login_response.status_code == 200  # Vérifie que la connexion réussit
 
-    # Test de réservation de places avec un club ayant suffisamment de points
+    login_response = client.post("/showSummary", data={"email": "john@simplylift.co"})
+    assert login_response.status_code == 200
+
     booking_response = client.post(
         "/purchasePlaces",
         data={"competition": "Spring Festival", "club": "Simply Lift", "places": 5},
@@ -21,15 +20,13 @@ def test_integration(client):
     assert booking_response.status_code == 200
     assert b"Great-booking complete!" in booking_response.data
 
-    # Test de réservation de places avec un club n'ayant pas suffisamment de points
-    # booking_response_enough_points = client.post(
-    #     "/purchasePlaces",
-    #     data={"competition": "Spring Festival", "club": "Iron Temple", "places": 5},
-    # )
-    # assert booking_response_enough_points.status_code == 200
-    # assert b"You don't have enough points!" in booking_response_enough_points.data
+    booking_response_enough_points = client.post(
+        "/purchasePlaces",
+        data={"competition": "Spring Festival", "club": "Iron Temple", "places": 5},
+    )
+    assert booking_response_enough_points.status_code == 200
+    assert b"have enough points!" in booking_response_enough_points.data
 
-    # Test de réservation de places pour une compétition passée
     booking_response_past_competition = client.post(
         "/purchasePlaces",
         data={"competition": "Fall Classic", "club": "Simply Lift", "places": 5},
